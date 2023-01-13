10dc238b9981c94ada83c8f8db2ac2ca456bf1d0
KunalDhruw <108332747+KunalDhruw@users.noreply.github.com>
2023-01-13 06:08:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,30 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            relatives: [
+                { key: 'relativeListItem1', name: 'Mom' },
+                { key: 'relativeListItem2', name: 'Dad' },
+                { key: 'relativeListItem3', name: 'Sister' },
+                { key: 'relativeListItem4', name: 'Brother' },
+            ],
+        };
+    }
     render() {
-
-        return(
+        return (
             <div id="main">
-               {/* Do not remove the main div */}
+                <h3>Diwali Shopping List</h3>
+                <ol key="relativeList">
+                    {this.state.relatives.map(relative => (
+                        <li key={relative.key}>{relative.name}</li>
+                    ))}
+                </ol>
             </div>
         )
     }
 }
-
 
 export default App;
